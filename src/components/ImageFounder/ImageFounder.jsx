46df6b5abd1456a56806734450a70213910080a8
Sergey <sergsmile29@gmail.com>
2023-03-09 09:24:32 +0200

    change css

--- src/components/ImageFounder/ImageFounder.jsx
+++ src/components/ImageFounder/ImageFounder.jsx
@@ -1,41 +1,21 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 import { Image } from './Image'
 import styled from 'styled-components'
 import { HeaderImgFounder } from './HeaderImgFounder'
-export  class ImageFounder extends Component {
-  state = {
-		modalIsOpen: false,
-		images: [],
+export class ImageFounder extends Component {
+	render() {
+		return (
+			<>
+				<HeaderImgFounder />
+				<ImageContainer></ImageContainer>
+			</>
+		)
 	}
-  handleOpenModel = () => {
-		this.setState(prevState => ({ modalIsOpen: !prevState.modalIsOpen }))
-	}
-
-	componentDidMount() {
-		axios
-			.get(
-				`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=yellow+flowers&image_type=photo`
-			)
-			.then(res => this.setState({ images: res.data.hits },console.log(this.state)))
-	}
-  render() {
-    const {images} = this.state
-    return (
-      <>
-      <HeaderImgFounder/>
-      <ImageContainer>
-      {images.map(image => <Image imageURL={image.largeImageURL}/>)}
-      </ImageContainer>
-      </>
-
-    )
-  }
 }
 const ImageContainer = styled.div`
-  display: grid;
-  padding: 20px 10px;
-  gap:20px;
-  grid-template-columns: repeat(auto-fill,minmax(500px, 1fr));
+	display: grid;
+	padding: 20px 10px;
+	gap: 20px;
+	grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
 `
