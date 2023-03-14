2ecfe6206bfe53d4df81d10c3e85a9bdc0e26522
Sergey <sergsmile29@gmail.com>
2023-03-14 13:29:34 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,20 +7,68 @@ import { ImageFounder } from './pages/ImageFounder'
 import { ColorPicker } from './pages/ColorPicker'
 import { NavBar } from './components/NavBar/NavBar'
 import { Vote } from './pages/Vote'
+import { createContext, useContext, useState } from 'react'
+
+const context = {
+	btnsVote: [
+		{ title: 'LINUX', name: 'linux' },
+		{ title: 'WINDOWS', name: 'windows' },
+		{ title: 'MAC OS', name: 'macOS' },
+	],
+	btnsCounter: [
+		{ title: 'dsaf', name: 'asdf' },
+		{ title: 'WINDOWS', name: 'windows' },
+		{ title: 'MAC OS', name: 'macOS' },
+	],
+	colorsData: [
+		{
+			id: 1,
+			color: 'white',
+		},
+		{
+			id: 2,
+			color: 'lightblue',
+		},
+		{
+			id: 3,
+			color: 'orange',
+		},
+		{
+			id: 4,
+			color: 'tomato',
+		},
+		{
+			id: 5,
+			color: 'teal',
+		},
+		{
+			id: 6,
+			color: 'yellow',
+		},
+	],
+	colorsTheme: {
+		mainColor: 'blue',
+		secondColor: 'red',
+	},
+}
+
+export const MyContext = createContext(context)
 export const App = () => {
 	return (
 		<BrowserRouter>
 			<NavBar />
 			<hr />
-			<Routes>
-				<Route index element={<ColorPicker />} />
-				<Route path='counter' element={<Counter />} />
-				<Route path='imageFounder' element={<ImageFounder />} />
-				<Route path='colorPicker' element={<ColorPicker />} />
-				<Route path='login' element={<LoginForm />} />
-				<Route path='todoList' element={<TodoList />} />
-				<Route path='vote' element={<Vote />} />
-			</Routes>
+			<MyContext.Provider value={context}>
+				<Routes>
+					<Route index element={<ColorPicker />} />
+					<Route path='counter' element={<Counter />} />
+					<Route path='imageFounder' element={<ImageFounder />} />
+					<Route path='colorPicker' element={<ColorPicker />} />
+					<Route path='login' element={<LoginForm />} />
+					<Route path='todoList' element={<TodoList />} />
+					<Route path='vote' element={<Vote />} />
+				</Routes>
+			</MyContext.Provider>
 		</BrowserRouter>
 	)
 }
--- src/pages/Vote/Vote.jsx
+++ src/pages/Vote/Vote.jsx
@@ -1,103 +1,149 @@
-import { Component, useEffect, useState } from 'react'
+import {
+	Component,
+	useContext,
+	useEffect,
+	useReducer,
+	useRef,
+	useState,
+} from 'react'
 import { ControlledButtons } from './ControlledButtons'
 import { Flex } from '../../components/Flex.styled'
 import styled from 'styled-components'
 
-let intervalId = null
-export const Vote = () => {
-	const [windowsValue, setWindowsValue] = useState(0)
-	const [linuxValue, setLinuxValue] = useState(0)
-	const [macOSValue, setMacOSValue] = useState(0)
-
-	// set time and reset
-	useEffect(() => {
-		console.log('useEffect is mount')
-		intervalId = setInterval(() => console.log(new Date()), 1000)
-
-		return () => {
-			console.log('useEffect is unmount')
-			clearInterval(intervalId)
-		}
-	}, [])
-
-	//
-
-	useEffect(() => {
-		console.log('useEffect windowsValue || linuxValue')
-	}, [windowsValue, linuxValue])
-
-	useEffect(() => {
-		if (linuxValue > 10) {
-			console.log('useEffect linuxValue > 10')
-		}
-	}, [linuxValue])
-
-	const mapState = {
-		windows: setWindowsValue,
-		linux: setLinuxValue,
-		macOS: setMacOSValue,
+export const Vote = ({ bgColor }) => {
+	const initialState = {
+		windowsValue: 0,
+		linuxValue: 22,
+		macOSValue: 0,
+		isActive: true,
+		name: 'Alex',
 	}
+
+	const countReducer = (state, action) => {
+		if (action.type)
+			switch (action.type) {
+				case 'windows':
+					return { ...state, windowsValue: state.windowsValue + action.payload }
+				case 'linux':
+					return { ...state, linuxValue: state.linuxValue + action.payload }
+				case 'macOS':
+					return { ...state, macOSValue: state.macOSValue + action.payload }
+				case 'clear':
+					return { ...initialState }
+				case 'changeName':
+					return { ...state, name: action.payload }
+
+				case 'default':
+					return {
+						windowsValue: 777,
+						linuxValue: 222,
+						macOSValue: 330,
+					}
+				case 'changeSatus':
+					return { ...state, isActive: false }
+				default:
+					throw new Error(`this type is not support ${action.type}`)
+			}
+	}
+
+	const [state, dispatch] = useReducer(countReducer, initialState)
+
+	// const firstRender = useRef(true)
+	// const intervalId = useRef(null)
+	// const myRef = useRef()
+	// const withoutRef = document.querySelector('.myH2')
+	// const refValue = useRef(0)
+	// // set time and reset
+	// // useEffect(() => {
+	// // 	console.log('useEffect is mount')
+	// // 	intervalId.current = setInterval(() => console.log(new Date()), 1000)
+
+	// // 	return () => {
+	// // 		console.log('useEffect is unmount')
+	// // 		clearInterval(intervalId.current)
+	// // 	}
+	// // }, [])
+
+	// //
+	// useEffect(() => {
+	// 	if (firstRender.current) {
+	// 		console.log('Перший рендер')
+	// 		firstRender.current = false
+	// 		return
+	// 	}
+	// })
+
+	// useEffect(() => {
+	// 	console.log('useEffect windowsValue || linuxValue')
+	// }, [windowsValue, linuxValue])
+
+	// useEffect(() => {
+	// 	if (linuxValue > 10) {
+	// 		console.log('useEffect linuxValue > 10')
+	// 	}
+	// }, [linuxValue])
+
+	// const mapState = {
+	// 	windows: setWindowsValue,
+	// 	linux: setLinuxValue,
+	// 	macOS: setMacOSValue,
+	// }
 
 	const vote = e => {
 		const { name } = e.target
-		// this.setState(prevState => ({
-		// 	[e.target.name]: prevState[e.target.name] + 1,
-		// }))
+		dispatch({ type: 'windows' })
+
 		// if (name === 'windows') {
-		// 	setWindowsValue(prevState => prevState + 1)
+		// 	// setWindowsValue(prevState => prevState + 1)
+		// 	dispatch({ type: 'windows' })
 		// }
 		// if (name === 'linux') {
-		// 	setLinuxValue(prevState => prevState + 1)
+		// 	dispatch({ type: 'linux' })
 		// }
 		// if (name === 'macOS') {
-		// 	setMacOSValue(prevState => prevState + 1)
+		// 	dispatch({ type: 'macOS' })
 		// }
-		mapState[name](prevState => prevState + 1)
+
+		// mapState[name](prevState => prevState + 1)
 	}
 	return (
 		<Flex center>
 			<VoteContainer>
 				<h2 className='text-center'>Vote</h2>
 				<VoteResults>
-					<h2>{linuxValue}</h2>
-					<h2>{windowsValue}</h2>
-					<h2>{macOSValue}</h2>
+					<h2 className='myH2'>{state.linuxValue}</h2>
+					<h2>{state.windowsValue}</h2>
+					<h2>{state.macOSValue}</h2>
 				</VoteResults>
-				<ControlledButtons vote={vote} />
+				{/* <ControlledButtons bgColor={bgColor} vote={vote} /> */}
+				<button onClick={() => dispatch({ type: 'windows', payload: 4 })}>
+					Windows
+				</button>
+				<button onClick={() => dispatch({ type: 'linux', payload: 2 })}>
+					Linux
+				</button>
+				<button onClick={() => dispatch({ type: 'macOS', payload: 9 })}>
+					MacOs
+				</button>
+				<button onClick={() => dispatch({ type: 'clear' })}>clear</button>
+				<button onClick={() => dispatch({ type: 'default' })}>default</button>
+				<button
+					onClick={() =>
+						dispatch({
+							type: 'changeName',
+							payload: 'this name is changed with useReducer',
+						})
+					}
+				>
+					changeName
+				</button>
+				<button onClick={() => dispatch({ type: 'changeSatus' })}>
+					changeSatus
+				</button>
 			</VoteContainer>
 		</Flex>
 	)
 }
-// export class Vote extends Component {
-// 	state = {
-// 		windows: 0,
-// 		linux: 0,
-// 		macOS: 0,
-// 	}
-
-// 	vote = e => {
-// 		// this.setState({ count: this.state.count + 1 })
-// 		this.setState(prevState => ({
-// 			[e.target.name]: prevState[e.target.name] + 1,
-// 		}))
-// 	}
-
-// 	render() {
-// 		return (
-// 			<Flex center>
-// 				<VoteContainer>
-// 					<h2 className='text-center'>Vote</h2>
-// 					<VoteResults>
-// 						<h2>{this.state.linux}</h2>
-// 						<h2>{this.state.windows}</h2>
-// 						<h2>{this.state.macOS}</h2>
-// 					</VoteResults>
-// 					<ControlledButtons vote={this.vote} />
-// 				</VoteContainer>
-// 			</Flex>
-// 		)
-// 	}
-// }
 
 const VoteContainer = styled.div`
 	background-color: black;
