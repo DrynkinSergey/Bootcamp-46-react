019606577b7f4a64d141713ffc32ccad3a70a543
Sergey <sergsmile29@gmail.com>
2023-03-16 00:30:43 +0200

    update btns

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Posts = () => {
 	const [posts, setPosts] = useState([])
@@ -8,22 +8,34 @@ export const Posts = () => {
 			.then(response => response.json())
 			.then(json => setPosts(json))
 	}, [])
+	const navigate = useNavigate()
+
 	return (
 		<div>
 			<h1>Posts</h1>
-			<div className='ml-2'>
-				<Link
-					to='/posts/new'
-					className=' text-sm block w-[20%] max-w-[25%] border border-black text-center hover:bg-cyan-500 hover:text-white py-2 px-2 mt-4  bg-cyan-300'
-				>
-					Create new post
-				</Link>
+			<div className='flex gap-4'>
+				<div className=' ml-2'>
+					<Link
+						to='/posts/new'
+						className=' text-sm  block border border-black text-center hover:bg-cyan-500  py-2 px-2 mt-4  bg-cyan-300'
+					>
+						Create new post
+					</Link>
+				</div>
+				<button className=' text-sm   border border-black text-center hover:bg-cyan-500  py-2 px-2 mt-4  bg-cyan-300'>
+					Go back
+				</button>
 			</div>
-			{posts.map(post => (
-				<li className='list-decimal ' key={post.id}>
-					<Link to={`/posts/${post.id}`}>{post.title}</Link>
-				</li>
-			))}
+
+			<ol className='list-decimal px-8 py-4'>
+				{posts.map(post => (
+					<li key={post.id}>
+						<Link to={`/posts/${post.id}`}>
+							<p className='hover:text-sky-400'>{post.title}</p>
+						</Link>
+					</li>
+				))}
+			</ol>
 		</div>
 	)
 }
