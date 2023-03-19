d6ca8bf402bce4a6d21ec58aa4f566e4d569072d
Sergey <sergsmile29@gmail.com>
2023-03-19 12:18:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Counter } from './pages/Counter'
 import { TodoList } from './pages/TodoList'
 import { NavBar } from './components/NavBar/NavBar'
+import { useCallback } from 'react'
 
-export const App = () => {
+export const App = ({ value }) => {
+	const a = useCallback(() => {
+		console.log()
+	}, [])
+	// xd01xr0 - const a = () => {}
+	// xd00xr1 - const a = () => {}
+
 	return (
-		<BrowserRouter>
+		<>
 			<NavBar />
 			<hr />
 			<Routes>
-				<Route path='/' element={<Counter />} />
-				<Route path='todoList' element={<TodoList />} />
+				<Route path='/' element={<Counter functionA={a} />} />
+				<Route path='todoList' element={<TodoList value={value} />} />
 			</Routes>
-		</BrowserRouter>
+		</>
 	)
 }
