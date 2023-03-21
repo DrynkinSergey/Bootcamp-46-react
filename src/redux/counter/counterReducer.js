eb4da5655e4b8717fb7652f4679fa645f89be42c
Sergey <sergsmile29@gmail.com>
2023-03-21 11:56:06 +0200

    counterSlice

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,49 +1,52 @@
-import { createAction, createReducer } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
 
-// 1 ЕТАП.........................
-export const increment = createAction('increment')
-export const decrement = createAction('decrement')
-export const reset = createAction('reset')
-export const changeStep = createAction('changeStep')
-//................................
-//3 ЕТАП - створення редьюсеру createReducer
-export const counterReducer = createReducer(
-	{ count: 0, step: 1 }, // - initial state
-	{
-		[increment]: (state, action) => {
-			console.log(action.payload)
-			// return { ...state, count: state.count + state.step } - immutable
-			state.count += state.step //- mutable
+export const counterSlice = createSlice({
+	name: '@@counter',
+	initialState: { count: 0, step: 1 },
+	reducers: {
+		increment(state, action) {
+			state.count += state.step
 		},
-		[decrement]: state => {
-			// return { ...state, count: state.count - state.step } - immutable
-			state.count -= state.step //- mutable
+		decrement(state, action) {
+			state.count -= state.step
 		},
-		[reset]: (state, action) => {
-			state = { count: 0, step: 1 }
+		reset(state, action) {
+			return { count: 0, step: 1 }
 		},
-		[changeStep]: (state, action) => {
+		changeStep(state, action) {
 			state.step = action.payload
 		},
-	}
-)
+		changeSte2(state, action) {
+			state.step = action.payload
+		},
+	},
+})
 
-//2 ЕТАП - заміна кейсів
-// export const counterReducer = (state = 0, action) => {
-// 	switch (action.type) {
-// 		case increment.type: {
-// 			return { ...state, count: state.count + state.step }
-// 		}
-// 		case decrement.type: {
-// 			return { ...state, count: state.count - state.step }
-// 		}
-// 		case reset.type: {
-// 			return { ...state, count: 0, step: 1 }
-// 		}
-// 		case changeStep.type: {
-// 			return { ...state, step: action.payload }
-// 		}
-// 		default:
-// 			return state
+console.log(counterSlice)
+export const { increment, decrement, reset, changeStep } = counterSlice.actions
+
+export const counterReducer = counterSlice.reducer
+
+// // 1 ЕТАП.........................
+// export const increment = createAction('increment')
+// export const decrement = createAction('decrement')
+// export const reset = createAction('reset')
+// export const changeStep = createAction('changeStep')
+// //................................
+// export const counterReducer = createReducer(
+// 	{ count: 0, step: 1 }, // - initial state
+// 	{
+// 		[increment]: (state, action) => {
+// 			state.count += state.step //- mutable
+// 		},
+// 		[decrement]: state => {
+// 			state.count -= state.step //- mutable
+// 		},
+// 		[reset]: (state, action) => {
+// 			state = { count: 0, step: 1 }
+// 		},
+// 		[changeStep]: (state, action) => {
+// 			state.step = action.payload
+// 		},
 // 	}
-// }
+// )
