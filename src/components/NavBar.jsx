519db18491083861cc44a0817e068b6877f46ea0
Sergey <sergsmile29@gmail.com>
2023-03-27 04:02:36 +0300

    added nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
+	const isLoggedIn = useSelector(state => state.isLoggedIn)
+	const user = useSelector(state => state.user.name)
 	return (
-		<header className='text-4xl text-white font-bold py-6 px-4 bg-cyan-600 flex justify-between gap-6'>
+		<header className='text-2xl text-white font-bold py-6 px-4 bg-cyan-600 flex justify-between gap-6'>
 			<div className='flex gap-4'>
 				<NavLink to='/'>Home</NavLink>
 				<NavLink to='/posts'>Posts</NavLink>
@@ -11,6 +14,10 @@ export const NavBar = () => {
 			<div>
 				<NavLink to='/login'>login</NavLink> |{' '}
 				<NavLink to='/registration'>signUp</NavLink>
+			</div>
+			<div className='flex gap-4'>
+				<h1>Welcome, {user}</h1>
+				<button className='border'>LogOut</button>
 			</div>
 		</header>
 	)
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,28 @@
 import React, { useContext } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { Navigate, useNavigate } from 'react-router-dom'
+import { logIn } from '../redux/auth/operations'
 export const LoginPage = () => {
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 	const handleSubmit = e => {
 		e.preventDefault()
 		const form = e.target
 		const email = form.email.value
 		const password = form.password.value
+		dispatch(logIn({ email, password }))
+		form.reset()
 	}
 	return (
 		<div>
 			<h1>login</h1>
 			<form className='flex gap-2 flex-col w-1/4 p-4' onSubmit={handleSubmit}>
-				<input className='border' name='email' placeholder='name' type='text' />
+				<input
+					className='border'
+					name='email'
+					placeholder='email'
+					type='text'
+				/>
 				<input
 					className='border'
 					name='password'
